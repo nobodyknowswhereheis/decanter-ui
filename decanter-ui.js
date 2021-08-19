b887c3b68b0d16d7a02c9024b260233144fdb3bd
Jesse Adams <jesse.adams@servicenow.com>
2021-08-19 14:52:54 -0400

    random updates

--- decanter-ui.js
+++ decanter-ui.js
@@ -9,13 +9,20 @@ const path = require('path');
 class DecanterUI extends HttpService {
     constructor(service_bus, class_name) {
         super(service_bus, class_name);
+        var self = this;
         this.static_content = path.resolve('./static');
-
+        self.registerRoute("GET", "/login_page", self.respondWithLogin, true);
+        self.attachRouter("/", self.router);
     }
     respondWith404(req, res, next) {
         var fullpath = path.join(this.static_content, '/404.html');
         return res.status(404).sendFile(fullpath);
     }
+    async respondWithLogin(req, res, next, self) {
+        var fullpath = path.join(self.static_content, '/login.html');
+        return res.status(200).sendFile(fullpath);
+    }
+
 
 }
 module.exports = DecanterUI;
